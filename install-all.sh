9665d65d46b4d4dbc8f4ce06aeed780a32c188ac
ndrnmnk <ndrnmnk1@gmail.com>
2024-02-04 18:14:25 +0200

    added instant_ngp_utils.py

--- install-all.sh
+++ install-all.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 git clone --recursive https://github.com/cvg/Hierarchical-Localization/
 cd Hierarchical-Localization/
 python -m pip install -e .
+cd ..
+mv instant_ngp_utils.py Hierarchical-Localization/instant_ngp_utils.py
