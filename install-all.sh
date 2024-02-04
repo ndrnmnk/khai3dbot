0666cd26a9cc725308806c7009ad41a3091a03dc
ndrnmnk <ndrnmnk1@gmail.com>
2024-02-04 18:00:07 +0200

    added install script

--- install-all.sh
+++ install-all.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+sudo apt install ffmpeg -y
+pip install -r requirements.txt
 cd instant-ngp
+pip install -r requirements.txt
 git clone --recursive https://github.com/cvg/Hierarchical-Localization/
 cd Hierarchical-Localization/
 python -m pip install -e .
-cd ../
-sudo apt install ffmpeg
